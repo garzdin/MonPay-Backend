8df0c465148151cbd446e8ecb054097608b289d0
Teodor Garzdin <tsg@hugelawn.com>
2017-01-11 15:28:03 +0100

    Connect to the appropriate database, based on where the app is hosted at.

--- api/settings.py
+++ api/settings.py
@@ -1,13 +1,21 @@
+from os import environ
 from datetime import timedelta
 
 # The secret key used for signing tokens. Keep this safe.
 SECRET = '1p294u3ejaskdnalkdu12ejan$spdokj+qe129u10emcn1hxemn1xz12ez1p9eu12'
 
 # Database settings needed in order for the ORM to connect
-DATABASE_USER = 'monpay'
-DATABASE_PASSWORD = 'monpayPASS!'
-DATABASE_HOST = 'localhost'
-DATABASE_NAME = 'monpaydb'
+if 'DATABASE_URL' in environ:
+    DATABASE_BASE_URL = environ['DATABASE_URL'].split('//')[1].split(':')
+    DATABASE_USER = DATABASE_BASE_URL[0]
+    DATABASE_PASSWORD = DATABASE_BASE_URL[1].split('@')[0]
+    DATABASE_HOST = DATABASE_BASE_URL[1].split('@')[1]
+    DATABASE_NAME = DATABASE_BASE_URL[2].split('/')[1]
+else:
+    DATABASE_USER = 'monpay'
+    DATABASE_PASSWORD = 'monpayPASS!'
+    DATABASE_HOST = 'localhost'
+    DATABASE_NAME = 'monpaydb'
 
 # Token related settings
 TOKEN_EXPIRATION = timedelta(hours=2)
