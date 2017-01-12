5931ebbe32e1f90dc33013a91f38febdb6abd646
Teodor Garzdin <tsg@hugelawn.com>
2017-01-12 17:14:48 +0100

    Shorter errors. Add codes.

--- api/controllers/account.py
+++ api/controllers/account.py
@@ -14,41 +14,49 @@ class AccountCreateResource(object):
         """Handles POST requests"""
         data = load(req.bounded_stream)
         if not 'email' in data or not 'password' in data:
-            raise HTTPBadRequest("Bad Request", "Email and password fields are required.")
+            raise HTTPBadRequest(description="Email and password required", code=1)
         user = User.select(lambda u: u.email==data['email'])
         if user:
-            raise HTTPConflict("Conflict", "A user with that email already exists.")
+            raise HTTPConflict(description="Email already exists", code=1)
         user = User(
             first_name=data['first_name'] if 'first_name' in data else "",
             last_name=data['last_name'] if 'last_name' in data else "",
             email=data['email'],
             password=data['password']
         )
-        resp.body = dumps({'message': "User created succeffully."})
+        resp.body = dumps({
+            "description": "User created succeffully",
+        })
 
 
 class AccountLoginResource(object):
     @db_session
     def on_post(self, req, resp):
         """Handels POST requests"""
-        data = load(req.bounded_stream)
+        try:
+            data = load(req.bounded_stream)
+        except ValueError:
+            raise HTTPBadRequest(description="Invalid request", code=1)
         if not 'email' in data or not 'password' in data:
-            raise HTTPBadRequest("Bad Request", "Email and password fields are required.")
+            raise HTTPBadRequest(description="Email and password required", code=1)
         user = User.get(email=data['email'])
         if not user:
-            raise HTTPNotFound("Not Found", "The user with the specified email was not found.")
+            raise HTTPNotFound(description="Email not found", code=1)
         if not user.password == data['password']:
-            raise HTTPForbidden("Forbidden", "The entered password is not correct.")
+            raise HTTPForbidden(description="Incorrect password", code=2)
         token_data = {
-            'iss': TOKEN_ISSUER,
-            'aud': TOKEN_AUDIENCE,
-            'iat': datetime.utcnow(),
-            'uid': user.id
+            "iss": TOKEN_ISSUER,
+            "aud": TOKEN_AUDIENCE,
+            "iat": datetime.utcnow(),
+            "uid": user.id
         }
         if not 'remember' in data:
             token_data['exp'] = datetime.utcnow() + TOKEN_EXPIRATION
         token = encode(token_data, SECRET)
-        resp.body = dumps({'message': "Logged in succeffully.", 'token': token})
+        resp.body = dumps({
+            "message": "Logged in succeffully",
+            "token": token
+        })
 
 
 class AccountResource(object):
@@ -58,10 +66,10 @@ class AccountResource(object):
         if req.uid:
             user = User.get(id=req.uid)
             resp.body = dumps({
-                'email': user.email,
-                'first_name': user.first_name,
-                'last_name': user.last_name,
-                'registered_on': str(user.created_on)
+                "email": user.email,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "registered_on": str(user.created_on)
             })
         else:
-            resp.body = dumps({'message': req.decode_error})
+            resp.body = dumps({"message": req.decode_error})
