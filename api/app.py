519ca369ae75b6f311910efd82d5b747d7f90d0d
Teodor Garzdin <tsg@hugelawn.com>
2017-01-13 22:19:14 +0100

    Don’t need an index.

--- api/app.py
+++ api/app.py
@@ -9,15 +9,7 @@ __all__ = ['app']
 db.bind('postgres', user=DATABASE_USER, password=DATABASE_PASSWORD, host=DATABASE_HOST, database=DATABASE_NAME)
 db.generate_mapping(create_tables=True)
 
-
-class IndexResource(object):
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        resp.status = HTTP_200
-        resp.body = dumps({'message': "Welcome to the API!"})
-
 app = API()
-app.add_route('/api/v1/', IndexResource())
 app.add_route('/api/v1/account/create', AccountCreateResource())
 app.add_route('/api/v1/account/login', AccountLoginResource())
 app.add_route('/api/v1/account/reset', AccountResetResource())
