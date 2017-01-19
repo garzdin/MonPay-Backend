f7e9b7e7f79c0475540476304a6544b3368497d8
Teodor Garzdin <tsg@hugelawn.com>
2017-01-19 17:42:12 +0100

    Initialize stripe and add the api key to the settings file.

--- api/app.py
+++ api/app.py
@@ -1,13 +1,16 @@
 from json import dumps
 from falcon import API, HTTP_200
+import stripe
 from models import db
-from settings import DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_NAME
+from settings import DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_NAME, STRIPE_API_KEY
 from controllers import *
 
 __all__ = ['app']
 
 db.bind('postgres', user=DATABASE_USER, password=DATABASE_PASSWORD, host=DATABASE_HOST, database=DATABASE_NAME)
 db.generate_mapping(create_tables=True)
+
+stripe.api_key = STRIPE_API_KEY
 
 app = API()
 app.add_route('/api/v1/account/create', AccountCreateResource())
--- api/settings.py
+++ api/settings.py
@@ -4,6 +4,9 @@ from datetime import timedelta
 # The secret key used for signing tokens. Keep this safe.
 SECRET = '1p294u3ejaskdnalkdu12ejan$spdokj+qe129u10emcn1hxemn1xz12ez1p9eu12'
 
+# The API key to use when making calls to Stripe
+STRIPE_API_KEY = "sk_test_yDWzXT12gNqPiJYmICXNd4Vw"
+
 # Database settings needed in order for the ORM to connect
 if 'DATABASE_URL' in environ:
     DATABASE_BASE_URL = environ['DATABASE_URL'].split('//')[1].split(':')
