f1ef50448abe623fec232156c5c5d60ac30cd337
Teodor Garzdin <teodorgarzedin@gmail.com>
2017-02-16 14:42:28 +0100

    Raise on token validation errors.

--- middleware/token.py
+++ middleware/token.py
@@ -9,14 +9,12 @@ __all__ = ['validate_token']
 def validate_token(req, resp, resource, param):
     token = req.get_header('Authorization')
     if not token:
-        raise HTTPBadRequest('Bad Request', 'Missing authorization token.')
+        raise HTTPBadRequest('Bad Request', 'Missing authorization token')
     try:
         decoded = decode(token, SECRET, audience=TOKEN_AUDIENCE)
     except ExpiredSignatureError as e:
-        req.uid = None
-        req.decode_error = str(e)
+        raise HTTPBadRequest('Bad Request', 'Token has expired')
     except DecodeError as e:
-        req.uid = None
-        req.decode_error = str(e)
+        raise HTTPBadRequest('Bad Request', 'Token could not be decoded')
     else:
         req.uid = int(decoded['uid'])
