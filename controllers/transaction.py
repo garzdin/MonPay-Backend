7013cbd2eb2b13bfd7836ccbfb15b71d48dff41a
Teodor Garzdin <teodorgarzedin@gmail.com>
2017-02-20 12:30:31 +0100

    Get the transactions controller to use the schema definitions.

--- controllers/transaction.py
+++ controllers/transaction.py
@@ -1,7 +1,9 @@
 from json import load, dumps
 from falcon import HTTPBadRequest, HTTPNotFound, before
 from middleware.token import validate_token
+from encoders.datetime import DateTimeEncoder
 from models.models import Transaction, session
+from models.schema import TransactionSchema
 
 __all__ = ['TransactionListResource', 'TransactionGetResource',
            'TransactionCreateResource', 'TransactionDeleteResource']
@@ -12,14 +14,8 @@ class TransactionListResource(object):
     def on_get(self, req, resp):
         """Handles GET requests"""
         transactions = session.query(Transaction).filter(Transaction.user == req.uid)
-        output = [{
-            "id": transaction.id,
-            "amount": transaction.amount,
-            "currency": transaction.currency,
-            "reason": transaction.reason,
-            "completed": transaction.completed
-        } for transaction in transactions]
-        resp.body = dumps({"status": True, "transactions": output})
+        schema = TransactionSchema(many=True)
+        resp.body = dumps({"transactions": schema.dump(transactions).data}, cls=DateTimeEncoder)
 
 
 class TransactionGetResource(object):
@@ -29,36 +25,27 @@ class TransactionGetResource(object):
         transaction = session.query(Transaction).filter(Transaction.user == req.uid, Transaction.id == id).first()
         if not transaction:
             raise HTTPNotFound(description="Transaction not found")
-        resp.body = dumps({"status": True, "transaction": {
-            "id": transaction.id,
-            "amount": transaction.amount,
-            "currency": transaction.currency,
-            "reason": transaction.reason,
-            "completed": transaction.completed
-        }})
+        schema = TransactionSchema()
+        resp.body = dumps({"transaction": schema.dump(beneficiary).data}, cls=DateTimeEncoder)
 
 
-class TransactionCreateResource(object): #TODO Find bank info from IBAN
+class TransactionCreateResource(object):
     @before(validate_token)
     def on_post(self, req, resp):
         try:
             data = load(req.bounded_stream)
         except ValueError:
             raise HTTPBadRequest(description="Invalid request")
-        if 'amount' not in data or 'currency' not in data or 'reason' not in data:
-            raise HTTPBadRequest(
-                description="Provide all needed required fields")
-        transaction = Transaction(**data)
+        schema = TransactionSchema()
+        result = schema.load(data)
+        if result.errors:
+            raise HTTPBadRequest(description=result.errors)
+        transaction = Transaction(**result.data)
         transaction.user = req.uid
         session.add(transaction)
         session.commit()
-        resp.body = dumps({"status": True, "transaction": {
-            "id": transaction.id,
-            "amount": transaction.amount,
-            "currency": transaction.currency,
-            "reason": transaction.reason,
-            "completed": transaction.completed
-        }})
+        result = schema.dump(transaction)
+        resp.body = dumps({"transaction": result.data}, cls=DateTimeEncoder)
 
 
 class TransactionDeleteResource(object):
