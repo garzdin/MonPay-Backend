dbb9952b6976f43aac50a6e965b53ea7ff1e9216
Teodor Garzdin <teodorgarzedin@gmail.com>
2017-03-04 15:43:46 +0100

    For debug purposes allow all request origins.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from controllers.currency import *
 
 __all__ = ['app']
 
-cors = CORS(allow_all_origins=True)
+cors = CORS(allow_all_origins=True, allow_all_headers=True, allow_all_methods=True)
 
 app = API(middleware=[cors.middleware])
 app.add_route('/api/v1/upload/file', FileUploadResource())
